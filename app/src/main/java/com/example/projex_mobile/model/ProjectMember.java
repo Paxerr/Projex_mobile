339e919df3dd0c77ec15ef231b4d3907c89966e4
paxerr <quyetnguyenst2005@gmail.com>
2026-04-23 21:17:12 +0700

    create model

---
+++ app/src/main/java/com/example/projex_mobile/model/ProjectMember.java
@@ -0,0 +1,23 @@
+package com.example.projex_mobile.model;
+
+// ProjectMember.java
+public class ProjectMember {
+    private int UserId;
+    private int ProjectId;
+    private String Role;
+    private String JoinedAt;
+
+    public ProjectMember() {}
+
+    public int getUserId() { return UserId; }
+    public void setUserId(int userId) { UserId = userId; }
+
+    public int getProjectId() { return ProjectId; }
+    public void setProjectId(int projectId) { ProjectId = projectId; }
+
+    public String getRole() { return Role; }
+    public void setRole(String role) { Role = role; }
+
+    public String getJoinedAt() { return JoinedAt; }
+    public void setJoinedAt(String joinedAt) { JoinedAt = joinedAt; }
+}
