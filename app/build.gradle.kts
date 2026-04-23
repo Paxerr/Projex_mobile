ef5af5ccf94752ab97c71a1777fc7dbed1c622de
paxerr <quyetnguyenst2005@gmail.com>
2026-04-23 21:01:59 +0700

    update git ignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,4 @@ dependencies {
     implementation("com.google.code.gson:gson:2.13.2")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-
 }
