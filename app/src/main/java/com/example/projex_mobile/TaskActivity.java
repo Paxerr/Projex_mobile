c63d6eb78959ad2350bffd97080c939288bc1ed3
DinhTienDat <dinhtiendat2105@gmail.com>
2026-05-01 20:37:11 +0700

    Thiết kế java cho home và notification; bổ sung recycle view.

--- app/src/main/java/com/example/projex_mobile/TaskActivity.java
+++ app/src/main/java/com/example/projex_mobile/TaskActivity.java
@@ -50,7 +50,7 @@ public class TaskActivity extends AppCompatActivity {
                         break;
                     case "In Progress":
                         // xử lý
-                        status.setText("TIn Progress");
+                        status.setText("In Progress");
                         break;
                     case "Done":
                         // xử lý
