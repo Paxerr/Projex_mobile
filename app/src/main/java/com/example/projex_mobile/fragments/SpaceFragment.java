97d7a770fc1265a9ef4d2551b19c7c105b896d22
DinhTienDat <dinhtiendat2105@gmail.com>
2026-05-09 23:04:24 +0700

    thêm trang space và notification

---
+++ app/src/main/java/com/example/projex_mobile/fragments/SpaceFragment.java
@@ -0,0 +1,30 @@
+package com.example.projex_mobile.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.projex_mobile.R;
+
+public class SpaceFragment extends Fragment {
+
+    public SpaceFragment() {
+    }
+
+    public static SpaceFragment newInstance() {
+        return new SpaceFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.space_list_fragment, container, false);
+    }
+}
