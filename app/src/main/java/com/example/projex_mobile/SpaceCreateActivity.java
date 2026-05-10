8f1de39639330727323fe6db7ac771ded6ec02a5
DinhTienDat <dinhtiendat2105@gmail.com>
2026-05-10 20:00:07 +0700

    thêm chuyển trang tạo không gian

--- app/src/main/java/com/example/projex_mobile/SpaceCreateActivity.java
+++ app/src/main/java/com/example/projex_mobile/SpaceCreateActivity.java
@@ -3,16 +3,16 @@ package com.example.projex_mobile;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatEditText;
 
 public class SpaceCreateActivity extends AppCompatActivity {
 
     private TextView btnHuy, btnTao;
-    private AppCompatEditText edtTenKhongGian, edtTenNguoiQuanLy, edtMoTaKhongGian;
+    private EditText edtTenKhongGian, edtTenNguoiQuanLy, edtMoTaKhongGian;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
