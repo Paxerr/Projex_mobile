e5baf85cc684d03e8a4c44cd3fa5339036b4db10
paxerr <quyetnguyenst2005@gmail.com>
2026-05-18 08:35:32 +0700

    update logic for login fragment

--- app/src/main/java/com/example/projex_mobile/api/ApiService.java
+++ app/src/main/java/com/example/projex_mobile/api/ApiService.java
@@ -2,12 +2,19 @@ package com.example.projex_mobile.api;
 
 import com.example.projex_mobile.objects.DashboardOverview;
 import com.example.projex_mobile.objects.RecentItem;
+import com.google.gson.JsonObject;
 import java.util.List;
+import java.util.Map;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface ApiService {
+    @POST("api/auth/login")
+    Call<JsonObject> login(@Body Map<String, String> body);
+
     @GET("api/dashboard/overview")
     Call<DashboardOverview> getDashboardOverview(@Header("Authorization") String token);
 
