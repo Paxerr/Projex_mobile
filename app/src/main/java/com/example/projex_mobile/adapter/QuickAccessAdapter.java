259cca2585fa192667b55e479f81a462d1c9f022
tdatd-icy <dinhtiendat2105@gmail.com>
2026-05-22 23:18:55 +0700

    Tối ưu cho xoay ngang (600 + 840)

--- app/src/main/java/com/example/projex_mobile/adapter/QuickAccessAdapter.java
+++ app/src/main/java/com/example/projex_mobile/adapter/QuickAccessAdapter.java
@@ -1,13 +1,13 @@
 package com.example.projex_mobile.adapter;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import androidx.core.content.ContextCompat;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,17 +18,31 @@ import java.util.List;
 
 public class QuickAccessAdapter extends RecyclerView.Adapter<QuickAccessAdapter.ViewHolder> {
 
-    private final List<QuickAccessItem> items;
+    private static final int TYPE_PORTRAIT = 0;
+    private static final int TYPE_LANDSCAPE = 1;
 
-    public QuickAccessAdapter(List<QuickAccessItem> items) {
+    private final List<QuickAccessItem> items;
+    private final int layoutMode;
+
+    public QuickAccessAdapter(List<QuickAccessItem> items, int layoutMode) {
         this.items = items;
+        this.layoutMode = layoutMode;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return layoutMode;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        int layoutRes = (viewType == TYPE_LANDSCAPE)
+                ? R.layout.home_quick_access
+                : R.layout.home_quick_access;
+
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.home_quick_access, parent, false);
+                .inflate(layoutRes, parent, false);
         return new ViewHolder(view);
     }
 
