6ba4ca64b6fe900bcf7bf3b10f223356b3b4aad7
tdatd-icy <dinhtiendat2105@gmail.com>
2026-06-07 20:24:05 +0700

    load recent

--- app/src/main/java/com/example/projex_mobile/objects/RecentItem.java
+++ app/src/main/java/com/example/projex_mobile/objects/RecentItem.java
@@ -4,68 +4,28 @@ public class RecentItem {
     private int id;
     private String title;
     private String message;
-    private String ticketCode;
     private String avatarText;
     private String timeAgo;
     private String status;
+
     public RecentItem() {
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    public String getAvatarText() { return avatarText; }
+    public void setAvatarText(String avatarText) { this.avatarText = avatarText; }
 
-    public String getMessage() {
-        return message;
-    }
+    public String getTimeAgo() { return timeAgo; }
+    public void setTimeAgo(String timeAgo) { this.timeAgo = timeAgo; }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getTicketCode() {
-        return ticketCode;
-    }
-
-    public void setTicketCode(String ticketCode) {
-        this.ticketCode = ticketCode;
-    }
-
-    public String getAvatarText() {
-        return avatarText;
-    }
-
-    public void setAvatarText(String avatarText) {
-        this.avatarText = avatarText;
-    }
-
-    public String getTimeAgo() {
-        return timeAgo;
-    }
-
-    public void setTimeAgo(String timeAgo) {
-        this.timeAgo = timeAgo;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
